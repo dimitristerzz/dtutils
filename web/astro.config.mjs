345e4cf9f99bc565902e5379539e5d7147689c78
Dimitris Terzidis <dterzidis2008@gmail.com>
2026-04-22 15:16:48 +0300

    Fix base path in Astro configuration

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   site: "https://dimitristerzz.github.io/dtutils/",
-  base: "/dtutils/",
+  base: "dtutils",
   server: {
     host: true,
     port: 8080
